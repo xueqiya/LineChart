101be481db480b9f94721e77e8719ee5aa87f11d
xueqi <xueqi@zjmedia.net>
2020-10-20 11:50:57 +0800

    feat: 另一种实现

--- app/src/main/java/com/apkmatrix/demo/linechart/MainActivity.java
+++ app/src/main/java/com/apkmatrix/demo/linechart/MainActivity.java
@@ -6,12 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LineChart lineChart;
+    //x轴坐标对应的数据
+    private final List<String> xValue = new ArrayList<>();
+    //y轴坐标对应的数据
+    private final List<Integer> yValue = new ArrayList<>();
+    //折线对应的数据
+    private final Map<String, Integer> value = new HashMap<>();
+    private ChartView chartview;
     private Button refresh;
 
     @Override
@@ -21,30 +30,43 @@ public class MainActivity extends AppCompatActivity {
 
         initView();
 
+        for (int i = 0; i < 12; i++) {
+            xValue.add((i + 1) + "月");
+            value.put((i + 1) + "月", (int) (Math.random() * 181 + 60));//60--240
+        }
+
+        for (int i = 0; i < 6; i++) {
+            yValue.add(i * 60);
+        }
+
+        ChartView chartView = (ChartView) findViewById(R.id.chartview);
+        chartView.setValue(value, xValue, yValue);
+
         refresh.setOnClickListener(view -> {
-            Map<String, Float> map = getData();
-            lineChart.startDraw(map);
+            Map<String, Double> map = getData();
+//            chartview.startDraw(map);
         });
     }
 
-    private Map<String, Float> getData() {
-        Map<String, Float> map = new LinkedHashMap<>();
+
+    private Map<String, Double> getData() {
+        Map<String, Double> map = new LinkedHashMap<>();
         String[] dates = new String[]{"周一", "周二", "周三", "周四", "周五", "周六", "周日"};
         int size = dates.length;
         for (int i = 0; i < size; i++) {
             if (i != size - 1) {
                 final double d = Math.random();
-                final float num = (int) (d * 100);
+                final double num = (d * 100);
                 map.put(dates[i], num);
             } else {
-                map.put(dates[i], 0f);
+                map.put(dates[i], 0.0);
             }
         }
         return map;
     }
 
     private void initView() {
-        lineChart = findViewById(R.id.lineChart);
+        chartview = findViewById(R.id.chartview);
         refresh = findViewById(R.id.refresh);
     }
 }
